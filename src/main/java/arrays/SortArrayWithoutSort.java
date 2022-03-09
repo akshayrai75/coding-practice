fe43d66579f70b022d4f327ba3e4cd58cabf3326
Priyadarshi <apriyadarshi@internal.tavisca.com>
2022-03-10 01:24:58 +0530

    sort arrays of 0s, 1s and 2s method 2.

--- src/main/java/arrays/SortArrayWithoutSort.java
+++ src/main/java/arrays/SortArrayWithoutSort.java
@@ -1,15 +1,17 @@
 package arrays;
 
+import java.util.*;
+
 public class SortArrayWithoutSort {
-    public static void sort_0_1_2_array(int[] arrayOf_0_1_2) throws Exception {
+    public static void sort_0_1_2_arrayMethod_1(int[] arrayOf_0_1_2) throws Exception {
         int arrLen = arrayOf_0_1_2.length;
-        if(arrLen > 0) {
+        if (arrLen > 0) {
             int low = 0, mid = 0, high = arrLen - 1;
             for (int i = 0; i <= high; i++) {
                 switch (arrayOf_0_1_2[i]) {
                     case 0:
                         swapArrElements(arrayOf_0_1_2, low, i);
-                        if(mid == low) mid++;
+                        if (mid == low) mid++;
                         low++;
                         break;
                     case 1:
@@ -19,16 +21,16 @@ public class SortArrayWithoutSort {
                     case 2:
                         swapArrElements(arrayOf_0_1_2, i, high);
                         high--;
-                        if(arrayOf_0_1_2[i] == 0) {
+                        if (arrayOf_0_1_2[i] == 0) {
                             swapArrElements(arrayOf_0_1_2, low, i);
-                            if(mid == low) mid++;
+                            if (mid == low) mid++;
                             low++;
                         }
-                        if(arrayOf_0_1_2[i] == 1) {
+                        if (arrayOf_0_1_2[i] == 1) {
                             swapArrElements(arrayOf_0_1_2, mid, i);
                             mid++;
                         }
-                        if(arrayOf_0_1_2[i] == 2) {
+                        if (arrayOf_0_1_2[i] == 2) {
                             swapArrElements(arrayOf_0_1_2, i, high);
                             high--;
                         }
@@ -40,7 +42,40 @@ public class SortArrayWithoutSort {
         }
     }
 
-    private static void swapArrElements (int arr[], int firstPos, int secondPos){
+    public static Integer[] sort_0_1_2_arrayMethod_2(Integer[] arrayOf_0_1_2) {
+        Map<Integer, Integer> count_of_each_val = new HashMap<>(Map.ofEntries(
+                Map.entry(0, 0),
+                Map.entry(1, 0),
+                Map.entry(2, 0)
+        ));
+        List<Integer> arrOfUnknowns = new ArrayList<>();
+        Integer[] arrOfKnown = {0, 1, 2};
+        List<Integer> listOfKnown = Arrays.asList(arrOfKnown);
+
+        for (Integer num : arrayOf_0_1_2) {
+            if (listOfKnown.contains(num)) {
+                Integer temp_count = count_of_each_val.get(num);
+                count_of_each_val.put(num, ++temp_count);
+            } else {
+                arrOfUnknowns.add(num);
+            }
+        }
+
+        List<Integer> sortedList = new ArrayList<>();
+
+        count_of_each_val.forEach((key, value) -> {
+            for (int i = 0; i < value; i++) sortedList.add(key);
+        });
+        sortedList.addAll(arrOfUnknowns);
+
+        arrayOf_0_1_2 = Arrays.stream(sortedList.toArray())
+                .map(o -> (Integer) o)
+                .toArray(Integer[]::new);
+
+        return arrayOf_0_1_2;
+    }
+
+    private static void swapArrElements(int arr[], int firstPos, int secondPos) {
         int temp = arr[firstPos];
         arr[firstPos] = arr[secondPos];
         arr[secondPos] = temp;
