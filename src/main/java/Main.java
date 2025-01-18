50820ff0676bc558c1b6dbaff61e1134cd84d7b2
Akshay Rai <akshayrai75@gmail.com>
2025-01-19 01:41:34 +0530

    Climbing Stairs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 import easy.ClaculateSqrt;
 import easy.ClimbingStairs;
+import easy.RemoveDuplicatesFromLL;
+import easy.mergeTwoSortedList.ListNode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ClimbingStairs.climbStairs(2));
-        System.out.println(ClimbingStairs.climbStairs(3));
-        System.out.println(ClimbingStairs.climbStairs(4));
-        System.out.println(ClimbingStairs.climbStairs(5));
+        ListNode test1 = new ListNode(1, new ListNode(1, new ListNode(2)));
+        ListNode test2 = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+        ListNode test3 = null;
+
+        test1 = RemoveDuplicatesFromLL.deleteDuplicates(test1);
+        test2 = RemoveDuplicatesFromLL.deleteDuplicates(test2);
+        test3 = RemoveDuplicatesFromLL.deleteDuplicates(null);
+
+        while (test1 != null) {
+            System.out.print(test1.val + " ");
+            test1 = test1.next;
+        }
+        System.out.println();
+        while (test2 != null) {
+            System.out.print(test2.val + " ");
+            test2 = test2.next;
+        }
+        System.out.println();
+        while (test3 != null) {
+            System.out.print(test3.val + " ");
+            test3 = test3.next;
+        }
     }
 }
---
+++ src/main/java/easy/RemoveDuplicatesFromLL.java
@@ -0,0 +1,33 @@
+package easy;
+
+import easy.mergeTwoSortedList.ListNode;
+
+public class RemoveDuplicatesFromLL {
+    /**
+     * <b>Remove Duplicates from Sorted List</b><br>
+     * Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+     * Return the linked list sorted as well.<br>
+     * @Example-1: Input: head = [1,1,2]<br>
+     * Output: [1,2]<br>
+     * @Example-2: Input: head = [1,1,2,3,3]<br>
+     * Output: [1,2,3]<br>
+     *
+     * */
+    public static ListNode deleteDuplicates(ListNode head) {
+        // This is two pointer approach and beats 100% in TC and 53.44% in SC
+        if (head==null) return null;
+
+        ListNode node1 = head, node2 = node1.next;
+
+        while (node2 != null) {
+            if (node2.val == node1.val) node2 = node2.next;
+            else {
+                node1.next = node2;
+                node1 = node2;
+                node2 = node2.next;
+            }
+        }
+        node1.next = node2;
+        return head;
+    }
+}
